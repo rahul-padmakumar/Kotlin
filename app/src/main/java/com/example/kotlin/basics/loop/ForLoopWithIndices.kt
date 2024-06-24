e5300d7544916619d6b136d169d3207e83c2979b
rahul <rahulp5875@gmail.com>
2024-06-24 18:45:21 +0530

    For loop with indices example

---
+++ app/src/main/java/com/example/kotlin/basics/loop/ForLoopWithIndices.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin.basics.loop
+
+fun main(){
+    val list = listOf(1, 2, 3, 4, 5)
+    for(i in list.indices){
+        println(list[i])
+    }
+
+    for((index, value) in list.withIndex()){
+        println("$index -> $value")
+    }
+
+    val array = arrayOf(1, 2, 3, 4, 5)
+    for(i in array.indices){
+        println(array[i])
+    }
+
+    for((index, value) in array.withIndex()){
+        println("$index -> $value")
+    }
+}
