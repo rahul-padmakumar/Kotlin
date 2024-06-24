75a213423623b781cc3cc0556daf9723cef523b6
rahul <rahulp5875@gmail.com>
2024-06-24 18:50:57 +0530

    Right half pyramid

---
+++ app/src/main/java/com/example/kotlin/pattern_printing/RightHalfPyramidPattern.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin.pattern_printing
+
+fun main(){
+
+    println("Enter the number of rows")
+    val maxRows = readlnOrNull()?.toIntOrNull() ?: 0
+
+    if(maxRows == 0){
+        println("Enter zero as row count")
+        return
+    }
+
+    for(i in 1..maxRows){
+        for(j in 1..i){
+            print("*\t")
+        }
+        println("")
+    }
+}
