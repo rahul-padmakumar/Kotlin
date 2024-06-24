da19f4400d0eca0a7909a582a4a220fa87a0906e
rahul <rahulp5875@gmail.com>
2024-06-24 21:00:10 +0530

    Full pyramid

---
+++ app/src/main/java/com/example/kotlin/pattern_printing/FullPyramidPattern.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin.pattern_printing
+
+fun main(){
+    println("Enter the number of rows")
+    val maxRows = readlnOrNull()?.toIntOrNull() ?: 0
+
+    if(maxRows == 0){
+        println("Enter zero as row count")
+        return
+    }
+    for(i in 1..maxRows){
+
+        var k = 1
+
+        for(j in 1..(maxRows - i)){
+            print("\t")
+        }
+        while (k <= (2 * i) - 1){
+            print("*\t")
+            k += 1
+        }
+        println()
+    }
+}
