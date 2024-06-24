427f55a975b22ae6f22df1acaf51bef48772dff7
rahul <rahulp5875@gmail.com>
2024-06-24 18:45:27 +0530

    For loop with ranges example

---
+++ app/src/main/java/com/example/kotlin/basics/loop/ForLoopWithRanges.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin.basics.loop
+
+fun main(){
+    for(i in 1..10){
+        println(i)
+    }
+
+    for(i in 10 downTo 1){
+        println(i)
+    }
+
+    for(i in 10 downTo 1 step 2){
+        println(i)
+    }
+
+    for(i in 1 until 10 step 3){
+        println(i)
+    }
+}
