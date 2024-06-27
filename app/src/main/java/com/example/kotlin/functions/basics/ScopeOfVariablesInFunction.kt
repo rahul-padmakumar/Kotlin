42504886cddbd662a90727e4991f8d496d4ca779
rahul <rahulp5875@gmail.com>
2024-06-27 11:06:26 +0530

    Add example for scope of variable in function

---
+++ app/src/main/java/com/example/kotlin/functions/basics/ScopeOfVariablesInFunction.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.functions.basics
+
+fun main(){
+    val y = 5
+    printValue()
+    println("Value of y in main is $y")
+}
+
+fun printValue(){
+    val y = 10
+    println("Value of y in printValue is $y")
+}
+
