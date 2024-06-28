cc09b5df2ac8aa98fcc613474e0529209a6e5d4f
rahul <rahulp5875@gmail.com>
2024-06-28 20:51:57 +0530

    Minor code clean up.

--- app/src/main/java/com/example/kotlin/functions/hof/function_types/NullableFunctionTypes.kt
+++ app/src/main/java/com/example/kotlin/functions/hof/function_types/NullableFunctionTypes.kt
@@ -2,7 +2,7 @@ package com.example.kotlin.functions.hof.function_types
 
 fun main(){
 
-    var sum: ((Int, Int) -> Int)?
+    val sum: ((Int, Int) -> Int)?
     sum = {a, b -> a + b}
     println(sum(8, 9))
 }
