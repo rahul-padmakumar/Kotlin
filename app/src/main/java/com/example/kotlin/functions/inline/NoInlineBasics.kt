2c99cfba20c1074bf83b927d24dbf2b206342085
rahul <rahulp5875@gmail.com>
2024-07-01 18:07:30 +0530

    Update example of no-inline basics.

---
+++ app/src/main/java/com/example/kotlin/functions/inline/NoInlineBasics.kt
@@ -0,0 +1,70 @@
+package com.example.kotlin.functions.inline
+
+/**
+ * By default all parameters of inline functions are inlined
+ * if we need to make some exceptional we can use noInline
+ */
+fun main(){
+    takeClass()
+    takeClass2()
+}
+
+/**
+ * String var0 = "Start class";
+ *       System.out.println(var0);
+ *       Function0 subj2$iv = (Function0)null.INSTANCE;
+ *       int $i$f$teachNoInline = false;
+ *       int var2 = false;
+ *       String var3 = "history";
+ *       System.out.println(var3);
+ *       subj2$iv.invoke();
+ *       var0 = "End class";
+ *       System.out.println(var0);
+ *
+ *  from above decompiled code first argument is called as inline while second argument is called as a normal function
+ */
+fun takeClass2() {
+    println("Start class")
+    teachNoInline(
+        { println("history") },
+        { println("Physics") }
+    )
+    println("End class")
+}
+
+
+/**
+ * public static final void takeClass() {
+ *       String var0 = "Start class";
+ *       System.out.println(var0);
+ *       int $i$f$teach = false;
+ *       int var1 = false;
+ *       String var2 = "history";
+ *       System.out.println(var2);
+ *       var1 = false;
+ *       var2 = "Physics";
+ *       System.out.println(var2);
+ *       var0 = "End class";
+ *       System.out.println(var0);
+ *    }
+ *    Both parameters are inlined
+ */
+fun takeClass(){
+    println("Start class")
+    teach(
+        { println("history") },
+        { println("Physics") }
+    )
+    println("End class")
+}
+
+inline fun teach(subj1: () -> Unit, subj2: () -> Unit){
+    subj1()
+    subj2()
+}
+inline fun teachNoInline(subj1: () -> Unit, noinline subj2: () -> Unit){
+    subj1()
+    subj2()
+}
+
+
