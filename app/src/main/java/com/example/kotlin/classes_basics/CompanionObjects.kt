b30b17f54102ab262fedd81c7d05ff380469d2dc
rahul <rahulp5875@gmail.com>
2024-07-16 16:24:21 +0530

    Add example of companion object.

---
+++ app/src/main/java/com/example/kotlin/classes_basics/CompanionObjects.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.classes_basics
+
+fun main(){
+    println(CompanionObjectEg.TYPE)
+    CompanionObjectEg.printMessage("Hello, all \nthis is me.")
+}
+
+class CompanionObjectEg{
+    companion object{
+        const val TYPE = "STATIC"
+        fun printMessage(message: String) = println(message)
+    }
+}
