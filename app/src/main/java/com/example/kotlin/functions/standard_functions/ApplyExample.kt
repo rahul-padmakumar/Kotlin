80104da553c3c27593778bd1a13bc091d78c76ed
rahul <rahulp5875@gmail.com>
2024-07-23 13:22:09 +0530

    Add example for apply function.

---
+++ app/src/main/java/com/example/kotlin/functions/standard_functions/ApplyExample.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin.functions.standard_functions
+
+/**
+ * Apply the following assignments to the object
+ */
+fun main(){
+    var person = ApplyPerson().apply {
+        name = "Tester"
+        age = 10
+        city = "Nashville"
+    }
+    person.printData()
+    person = ApplyPerson().apply {
+        name = "Tester1"
+        age = 27
+        city = "North Pole"
+    }
+    person.printData()
+}
+
+private class ApplyPerson{
+    var name: String = ""
+    var age: Int = 0
+    var city: String = ""
+
+    fun printData(){
+        println("Name - $name, age = $age, city = $city")
+    }
+}
+
+private inline fun <T> T.applyExample(block: T.() -> T): T = block()
