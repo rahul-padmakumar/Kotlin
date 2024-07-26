d0b3c3975564d9dcde58b5937e47de84140c3e48
rahul <rahulp5875@gmail.com>
2024-07-26 12:18:20 +0530

    Add example of sealed classes.

---
+++ app/src/main/java/com/example/kotlin/classes_basics/sealed_classes/SealedClassBasics.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin.classes_basics.sealed_classes
+
+private sealed class Shape{
+    class Circle(radius: Int): Shape()
+
+    data class Triangle(val base: Int, val height: Int, val angle: Int): Shape()
+}
+
+private data object NoShape: Shape()
+
+fun main(){
+    listOf(
+        Shape.Circle(12),
+        Shape.Triangle(10, 5, 60)
+    ).forEach {
+        when(it){
+            is Shape.Circle -> println("Circle")
+            is Shape.Triangle -> println("Triangle")
+            is NoShape -> println()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kotlin/classes_basics/sealed_classes/SealedInterface.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin.classes_basics.sealed_classes
+
+sealed interface IQuadrilateral
+
+class Square(length: Int): IQuadrilateral
+
+data class Rectangle(val length: Int, val breadth: Int): IQuadrilateral
+
+fun main(){
+    listOf(
+        Square(100),
+        Rectangle(50, 50)
+    ).forEach {
+        when(it){
+            is Square -> println("Square")
+            is Rectangle -> println("Rectangle")
+        }
+    }
+}
+
