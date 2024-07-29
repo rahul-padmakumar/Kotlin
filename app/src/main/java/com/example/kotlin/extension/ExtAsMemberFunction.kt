8947f001162cc84aa04fb2c2df5ce3cceb6abfff
rahul <rahulp5875@gmail.com>
2024-07-29 15:49:44 +0530

    Add example for extension function as member function,

---
+++ app/src/main/java/com/example/kotlin/extension/ExtAsMemberFunction.kt
@@ -0,0 +1,25 @@
+package com.example.kotlin.extension
+
+class Host(private val hostName: String){
+    fun printHostName() = print("Host name: $hostName")
+}
+
+class Connection(private val host: Host, private val port: Int){
+
+    private fun printPort() = println("Port number: $port")
+
+    private fun Host.printConnection(){
+        printHostName()
+        printPort()
+        println(toString())
+        println(this@Connection.toString())
+    }
+
+    fun connect(){
+        host.printConnection()
+    }
+}
+
+fun main(){
+    Connection(Host("google.com"), port = 1234).connect()
+}
