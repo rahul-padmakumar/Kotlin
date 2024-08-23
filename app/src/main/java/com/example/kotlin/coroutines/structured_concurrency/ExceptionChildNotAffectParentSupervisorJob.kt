bc0cc594e391016bcd007504c82707cc0a50aa3b
rahul <rahulp5875@gmail.com>
2024-08-23 14:53:07 +0530

    Minor code clean up.

--- app/src/main/java/com/example/kotlin/coroutines/structured_concurrency/ExceptionChildNotAffectParentSupervisorJob.kt
+++ app/src/main/java/com/example/kotlin/coroutines/structured_concurrency/ExceptionChildNotAffectParentSupervisorJob.kt
@@ -2,7 +2,6 @@ package com.example.kotlin.coroutines.structured_concurrency
 
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
