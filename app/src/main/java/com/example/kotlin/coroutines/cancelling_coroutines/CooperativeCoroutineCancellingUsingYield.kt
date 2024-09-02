28fb47da1ea34f8008599389081979120fb467b5
rahul <rahulp5875@gmail.com>
2024-09-02 10:12:00 +0530

    Add example for yield.

---
+++ app/src/main/java/com/example/kotlin/coroutines/cancelling_coroutines/CooperativeCoroutineCancellingUsingYield.kt
@@ -0,0 +1,27 @@
+package com.example.kotlin.coroutines.cancelling_coroutines
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
+fun main(){
+
+    println("Programme: Starting")
+    runBlocking {
+        val job = launch(Dispatchers.Default) {
+            repeat(10){
+                yield()
+                Thread.sleep(it * 1000L)
+                println("Coroutine - $it")
+            }
+        }
+        job.invokeOnCompletion {
+            println("scope cancelled")
+        }
+        delay(3000L)
+        println("Cancelling coroutine")
+        job.cancel()
+    }
+}
