777200f94a833fa5301a0c74a709aa7c3e98b601
rahul <rahulp5875@gmail.com>
2024-09-02 11:40:50 +0530

    Add example for coroutine exception handling using exception handler in root.

---
+++ app/src/main/java/com/example/kotlin/coroutines/exception_handling/CoroutineExceptionHandlerInRoot.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin.coroutines.exception_handling
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.lang.RuntimeException
+
+fun main(){
+    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        println("Exception - ${throwable.message}")
+    }
+    val scope = CoroutineScope(Job())
+    scope.launch (exceptionHandler){
+        launch {
+            throw RuntimeException("Coroutine Failed")
+        }
+    }
+    Thread.sleep(1000L)
+}
