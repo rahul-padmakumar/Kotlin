e2339fb8fbe8500033f732365e02dc753c4f1374
rahul <rahulp5875@gmail.com>
2024-09-02 10:05:20 +0530

    Add example for non cooperative cancelling.

---
+++ app/src/main/java/com/example/kotlin/coroutines/cancelling_coroutines/NonCooperativeCancelling.kt
@@ -0,0 +1,27 @@
+package com.example.kotlin.coroutines.cancelling_coroutines
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(){
+
+    println("Programme: Starting")
+    runBlocking {
+        val job = launch(Dispatchers.Default) {
+            repeat(10){
+                Thread.sleep(it * 1000L)
+                println("Coroutine - $it")
+            }
+        }
+        job.invokeOnCompletion {
+            println("scope cancelled")
+        }
+        delay(3000L)
+        println("Cancelling coroutine")
+        job.cancel()
+    }
+}
