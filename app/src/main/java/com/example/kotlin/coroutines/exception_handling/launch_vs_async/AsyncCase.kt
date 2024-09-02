4f86ae9be1449a64b0498851ef6caec45b744a80
rahul <rahulp5875@gmail.com>
2024-09-02 20:17:49 +0530

    Minor code change.

--- app/src/main/java/com/example/kotlin/coroutines/exception_handling/launch_vs_async/AsyncCase.kt
+++ app/src/main/java/com/example/kotlin/coroutines/exception_handling/launch_vs_async/AsyncCase.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 fun main(){
     val exceptionHandler = CoroutineExceptionHandler{_, throwable ->
