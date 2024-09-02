df241084b7cae90abe4d3dccd24a322235b09f56
rahul <rahulp5875@gmail.com>
2024-09-02 10:43:17 +0530

    Add example for error handling with try catch.

---
+++ app/src/main/java/com/example/kotlin/coroutines/exception_handling/TryCatchWithCoroutine.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin.coroutines.exception_handling
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.lang.RuntimeException
+
+fun main(){
+    val scope = CoroutineScope(Job())
+    scope.launch {
+        launch {
+            try {
+                throw RuntimeException("Coroutine Failed")
+            } catch (e: Exception){
+                println("Exception occurred: ${e.message}")
+            }
+        }
+    }
+    Thread.sleep(1000L)
+}
