531e71add379060a9c99f1a5c22d44027ccfb7d3
rahul <rahulp5875@gmail.com>
2024-09-03 16:44:49 +0530

    Add example for filter operator.

---
+++ app/src/main/java/com/example/kotlin/flow/non_terminal_operators/FilterOperator.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin.flow.non_terminal_operators
+
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+fun main(){
+    runBlocking {
+        flowOf(20, 2, 30, 4, 50, 6)
+            .filter { it > 10 }
+            .onEach { println(it) }
+            .launchIn(this)
+    }
+}
