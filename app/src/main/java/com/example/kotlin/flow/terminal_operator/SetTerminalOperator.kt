8349d306c2b568d289ad99d0c5f5ac84298f999d
rahul <rahulp5875@gmail.com>
2024-09-03 10:49:16 +0530

    Add example for toSet terminal operator.

---
+++ app/src/main/java/com/example/kotlin/flow/terminal_operator/SetTerminalOperator.kt
@@ -0,0 +1,25 @@
+package com.example.kotlin.flow.terminal_operator
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.toSet
+import kotlinx.coroutines.runBlocking
+
+fun main(){
+    val flow = flow {
+        println("Emitting first item")
+        delay(1000)
+        emit(1)
+        println("Emitting second item")
+        delay(1000)
+        emit(2)
+        println("Emitting third item")
+        delay(1000)
+        emit(2)
+    }
+
+    runBlocking {
+        println(flow.toSet())
+    }
+}
