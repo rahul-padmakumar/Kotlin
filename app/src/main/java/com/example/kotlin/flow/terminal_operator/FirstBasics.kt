2bdef9a941e8f944508c19c85495c5889182ba22
rahul <rahulp5875@gmail.com>
2024-09-03 10:43:59 +0530

    Add example for first terminal operator.

---
+++ app/src/main/java/com/example/kotlin/flow/terminal_operator/FirstBasics.kt
@@ -0,0 +1,30 @@
+package com.example.kotlin.flow.terminal_operator
+
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Flow builder will run on coroutine in which flow collector is called
+ * In case of first after receiving first value flow will be cancelled.
+ */
+
+fun main(){
+
+    val flow = flow {
+        println("Emitting first item")
+        kotlinx.coroutines.delay(1000)
+        emit(1)
+        println("Emitting second item")
+        kotlinx.coroutines.delay(1000)
+        emit(2)
+    }
+
+    val flow2 = flow<Int> {}
+
+    runBlocking {
+        println(flow.first())
+        println(flow2.firstOrNull())
+    }
+}
