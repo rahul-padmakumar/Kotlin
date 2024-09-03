dfe69cd19aa7b272b24308366e7c8b024d50044a
rahul <rahulp5875@gmail.com>
2024-09-03 10:50:54 +0530

    Add example for fold terminal operator.

---
+++ app/src/main/java/com/example/kotlin/flow/terminal_operator/FoldTerminalOperator.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin.flow.terminal_operator
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+
+fun main(){
+    val flow = flow {
+        println("Emitting first item")
+        delay(1000)
+        emit(1)
+        println("Emitting second item")
+        delay(1000)
+        emit(2)
+    }
+
+    runBlocking {
+        println(flow.fold(20, {acc, value -> acc + value}))
+    }
+}
