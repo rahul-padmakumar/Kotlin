6546af38f0e2e11587a3fa3206b3eb4131c22bf6
rahul <rahulp5875@gmail.com>
2024-09-03 17:29:11 +0530

    Add example for combine transform operator.

--- app/src/main/java/com/example/kotlin/flow/non_terminal_operators/zip/CombineTransformOperator.kt
+++ app/src/main/java/com/example/kotlin/flow/non_terminal_operators/zip/CombineTransformOperator.kt
@@ -1,7 +1,7 @@
 package com.example.kotlin.flow.non_terminal_operators.zip
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.combineTransform
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -10,10 +10,11 @@ import kotlinx.coroutines.runBlocking
 fun main(){
     runBlocking {
         flowOf(1, 2).onEach { delay(400) }
-            .combine(
+            .combineTransform(
                 flowOf("A", "B", "C").onEach { delay(300) }
             ){v1, v2 ->
-                "$v1$v2"
+                emit("Processing in progress")
+                emit("$v1$v2")
             }.onEach {
                 println(it)
             }.launchIn(this)
