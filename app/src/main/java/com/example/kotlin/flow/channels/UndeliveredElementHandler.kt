9c5404a95e73140d50fea58dc1488bad07c1056f
rahul <rahulp5875@gmail.com>
2024-09-07 15:16:52 +0530

    Add example of undelivered element handler.

---
+++ app/src/main/java/com/example/kotlin/flow/channels/UndeliveredElementHandler.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin.flow.channels
+
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(){
+    runBlocking {
+        val channel = Channel<Int>(onBufferOverflow = BufferOverflow.DROP_OLDEST){
+            println("Undelivered element: $it")
+        }
+        launch {
+            repeat(10){
+                delay(1000)
+                println("Emit $it")
+                channel.send(it)
+            }
+        }
+        launch {
+            channel.consumeEach{
+                println("Collect $it")
+                delay(5000)
+            }
+        }
+        delay(4000)
+        channel.close()
+    }
+}
